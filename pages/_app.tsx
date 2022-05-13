d20af7b528638c0232aa5c7b5a5d23a4364cb497
miyabin66 <freezemark5166@gmail.com>
2022-05-13 16:55:52 +0900

    修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  
+  h1,
+  h2 {
+    font-weight: bold;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
