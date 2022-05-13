ddcb21a1e9b054815f951b3da09a50164eb0e3c1
miyabin66 <freezemark5166@gmail.com>
2022-05-13 16:53:45 +0900

    styled componentsで再実装

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ import Document, {
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
